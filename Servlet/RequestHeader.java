c657d2aa37bb8a419de893359e1d6a7f1025c11f
mrsreehari <87757923+mrsreehari@users.noreply.github.com>
2021-08-01 11:43:38 +0530

    Add files via upload

---
+++ Servlet/RequestHeader.java
@@ -0,0 +1,31 @@
+package com.ust.servletpgms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestHeader
+ */
+@WebServlet("/RequestHeader")
+public class RequestHeader extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Enumeration e = request.getHeaderNames();
+        while (e.hasMoreElements()) {
+            String name = (String)e.nextElement();
+            String value = request.getHeader(name);
+            out.println(name + " = " + value);
+        }
+    }
+}
---
+++ Servlet/Sessions.java
@@ -0,0 +1,54 @@
+package com.ust.servletpgms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Sessions
+ */
+@WebServlet("/Sessions")
+public class Sessions extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        HttpSession session = request.getSession(true);
+
+        // print session info
+
+        Date created = new Date(session.getCreationTime());
+        Date accessed = new Date(session.getLastAccessedTime());
+        out.println("ID " + session.getId());
+        out.println("Created: " + created);
+        out.println("Last Accessed: " + accessed);
+
+        // set session info if needed
+
+        String dataName = request.getParameter("dataName");
+        if (dataName != null && dataName.length() > 0) {
+            String dataValue = request.getParameter("dataValue");
+            session.setAttribute(dataName, dataValue);
+        }
+
+        // print session contents
+
+        Enumeration e = session.getAttributeNames();
+        while (e.hasMoreElements()) {
+            String name = (String)e.nextElement();
+            String value = session.getAttribute(name).toString();
+            out.println(name + " = " + value);
+        }
+    }
+}
