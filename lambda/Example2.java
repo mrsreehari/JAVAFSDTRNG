940ae38e2512ff2a094aec257e11d0a855d81a59
mrsreehari <87757923+mrsreehari@users.noreply.github.com>
2021-08-05 11:21:28 +0530

    Add files via upload

---
+++ lambda/Example2.java
@@ -0,0 +1,25 @@
+package com.ust.lambda;
+
+import java.lang.FunctionalInterface;
+
+//this is functional interface
+@FunctionalInterface
+interface MyInterface{
+
+ // abstract method
+ double getPiValue();
+}
+
+public class Example2 {
+
+ public static void main( String[] args ) {
+	// declare a reference to MyInterface
+	    MyInterface ref;
+	    
+	    // lambda expression
+	    ref = () -> 3.1415;
+	    
+	    System.out.println("Value of Pi = " + ref.getPiValue());
+	    } 
+	}
+
